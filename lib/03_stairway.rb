6f848476300f6ebc999f845051c24b63cf68e018
Thib <55756142+Mariollet@users.noreply.github.com>
2019-10-03 17:34:05 +0200

    Add files via upload

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -13,7 +13,9 @@ def game ()
 		a = gets
 		puts
 		puts "Lancez de dé !"
-		puts "Dès de #{dice}"
+		dice = throw_dice
+		puts
+		puts "Dé de #{dice}"
 		if dice == 5 || dice == 6
 		 	puts "Vous avancez d'une marche !"
 		 	scores += 1 
@@ -29,50 +31,41 @@ def game ()
 		 	puts "Vous êtes à la marches #{scores}"
 		 	stat += 1
 		end
-	dice = throw_dice
+
+
 	end
 	puts
-	puts "Félicitaions vous avez réussi !"
-	puts "Vous avez fait #{stat} lancés !"
+	puts " Félicitaions vous avez réussi avec #{stat} lancés !"
+	print
 end
 
 def average_finish_time
 	stats_all = []
-	sim = 100
+	sim = 111
 	sim.times do
 		scores = 0
 		stat = 0
 		dice = throw_dice
 		until scores > 9 do
-			puts
-			puts "Lancez de dé !"
-			puts "Dès de #{dice}"
+			dice = throw_dice
 			if dice == 5 || dice == 6
-			 	puts "Vous avancez d'une marche !"
 			 	scores += 1 
 			 	stat += 1
-			 	puts "Vous êtes à la marches #{scores}"
 			elsif dice == 1
-			 	puts "Vous descendez d'une marche !"
 			 	scores -= 1
 			 	stat += 1
-			 	puts "Vous êtes à la marches #{scores}"
 			else
-			 	puts "Vous restez sur place !"
-			 	puts "Vous êtes à la marches #{scores}"
 			 	stat += 1
 			end
-		dice = throw_dice
 		end
 		puts
-		puts "Félicitaions vous avez réussi !"
-		puts "Vous avez fait #{stat} lancés !"
+		puts "Félicitaions vous avez réussi avec #{stat} lancés !"
 		stats_all << stat
 	end
 puts
 puts stats_all.inspect
 puts
-puts "En moyenne, il faut #{(stats_all.inject(0, :+) / sim )} lancés de dés pour finir ce jeu !"
+puts " En moyenne, il faut #{(stats_all.inject(0, :+) / sim )} lancés de dés pour finir ce jeu !"
 end
 
-average_finish_time
+game
